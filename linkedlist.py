e0b6740b8398590de19995b83a071a7bb7ebe3f8
sindhupriyakankanala <138544178+sindhupriyakankanala@users.noreply.github.com>
2023-08-30 19:11:25 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -77,62 +77,62 @@
 
 
 
-class Node:
-    def __init__(self,data):
-        self.data=data
-        self.next=None
+# class Node:
+#     def __init__(self,data):
+#         self.data=data
+#         self.next=None
 
-class LinkedList:
-    def __init__(self):
-        self.head=None
+# class LinkedList:
+#     def __init__(self):
+#         self.head=None
 
-    def insertion_at_beginning(self,data):
-        nb=Node(data)
-        nb.next=self.head
-        self.head=nb
+#     def insertion_at_beginning(self,data):
+#         nb=Node(data)
+#         nb.next=self.head
+#         self.head=nb
 
-    def insertion_at_end(self,data):
-        ne=Node(data)
-        temp=self.head
-        while temp.next:
-            temp=temp.next
-        temp.next=ne
+#     def insertion_at_end(self,data):
+#         ne=Node(data)
+#         temp=self.head
+#         while temp.next:
+#             temp=temp.next
+#         temp.next=ne
 
-    def insertion_at_pos(self,pos,data):
-        np=Node(data)
-        if pos==0
-           nb.next=self.head
-           self.head=nb
-        else:   
-           temp=self.head
-           for i in range(pos-1):
-              temp=temp.next
-           np.data=data
-           np.next=temp.next
-           temp.next=np
+#     def insertion_at_pos(self,pos,data):
+#         np=Node(data)
+#         if pos==0
+#            nb.next=self.head
+#            self.head=nb
+#         else:   
+#            temp=self.head
+#            for i in range(pos-1):
+#               temp=temp.next
+#            np.data=data
+#            np.next=temp.next
+#            temp.next=np
 
-    def display(self):
-        if self.head is None:
-            print("list is empty")
-        else:
-            temp=self.head
-            while temp:
-               print(temp.data,"-->",end="")
-               temp=temp.next
+#     def display(self):
+#         if self.head is None:
+#             print("list is empty")
+#         else:
+#             temp=self.head
+#             while temp:
+#                print(temp.data,"-->",end="")
+#                temp=temp.next
 
-l=LinkedList()
-n1=Node(10)
-l.head=n1
-n2=Node(20)
-n1.next=n2
-l.insertion_at_beginning(1)
-l.insertion_at_beginning(3)
-l.insertion_at_beginning(4)
-l.insertion_at_end(30)
-l.insertion_at_end(40)
-l.insertion_at_end(50)
-l.insertion_at_pos(4,25)
-l.display()
+# l=LinkedList()
+# n1=Node(10)
+# l.head=n1
+# n2=Node(20)
+# n1.next=n2
+# l.insertion_at_beginning(1)
+# l.insertion_at_beginning(3)
+# l.insertion_at_beginning(4)
+# l.insertion_at_end(30)
+# l.insertion_at_end(40)
+# l.insertion_at_end(50)
+# l.insertion_at_pos(4,25)
+# l.display()
 
 
 
